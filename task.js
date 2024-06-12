b659a8de85de408ec0c6f86eb5cfc97cdf1c38eb
CiprianBoamba <ciprian.boamba@gmail.com>
2024-06-12 14:46:29 +0300

    make first task

--- task.js
+++ task.js
@@ -1,1 +1,10 @@
-console.log('test');
+/** Task 1
+Create a function that will halve a given number.*/
+
+function halveNumber(num) {
+  return num / 2;
+}
+
+let result = halveNumber(-7.5);
+
+console.log(result);
